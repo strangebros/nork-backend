9662c23694118e9d462e8d3247bf162cb9084d2d
gmelon <hsh1769@naver.com>
2024-05-13 20:35:04 +0900

    refactor: JWTProvider에서 주입된 clock을 통해 현재 시간을 얻도록 변경, 테스트 코드 작성.

--- src/main/java/site/strangebros/nork/global/auth/exception/InvalidJWTException.java
+++ src/main/java/site/strangebros/nork/global/auth/exception/InvalidJWTException.java
@@ -2,11 +2,14 @@ package site.strangebros.nork.global.auth.exception;
 
 public class InvalidJWTException extends RuntimeException {
 
-    public InvalidJWTException(String message, Throwable cause) {
-        super(message, cause);
+    public InvalidJWTException() {
     }
 
     public InvalidJWTException(String message) {
         super(message);
+    }
+
+    public InvalidJWTException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
--- src/main/java/site/strangebros/nork/global/auth/utils/JWTProvider.java
+++ src/main/java/site/strangebros/nork/global/auth/utils/JWTProvider.java
@@ -1,37 +1,45 @@
 package site.strangebros.nork.global.auth.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import site.strangebros.nork.global.auth.exception.InvalidJWTException;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
-import java.time.Instant;
-import java.util.Base64;
-import java.util.Date;
-
+@RequiredArgsConstructor
 @Component
 public class JWTProvider implements InitializingBean {
     private static final int ACCESS_TOKEN_EXPIRATION_PERIOD = 60 * 30;
     private static final String MEMBER_ID_KEY = "memberId";
 
     @Value("${jwt.key}")
-    private String rawKey;
+    private String encodedKeyValue;
     private SecretKey key;
+
+    private final Clock clock;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        byte[] decodedKey = Base64.getDecoder().decode(rawKey);
-        key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+        key = buildKey();
+    }
+
+    private SecretKey buildKey() {
+        byte[] decodedKeyValue = Base64.getDecoder().decode(encodedKeyValue);
+        return Keys.hmacShaKeyFor(decodedKeyValue);
     }
 
     public String buildAccessToken(Integer memberId) {
-        Instant now = Instant.now();
+        Instant now = clock.instant();
 
         return Jwts.builder()
                 .claim(MEMBER_ID_KEY, memberId)
@@ -57,8 +65,10 @@ public class JWTProvider implements InitializingBean {
                     .build()
                     .parseSignedClaims(token)
                     .getPayload();
+        } catch (ExpiredJwtException exception) {
+            throw new InvalidJWTException("만료된 token 입니다.", exception);
         } catch (JwtException exception) {
-            throw new InvalidJWTException("잘못된 token 입니다.");
+            throw new InvalidJWTException("유효하지 않은 token 입니다.", exception);
         }
         return payload;
     }
