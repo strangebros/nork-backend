5b1fd33333030f5643daa4e3c668a82667f3a6f4
gmelon <hsh1769@naver.com>
2024-05-13 15:24:21 +0900

    chore: sql 파일 작성

---
+++ src/main/resources/sql/nork.sql
@@ -0,0 +1,96 @@
+CREATE TABLE `keyword`
+(
+    `id`    int NOT NULL,
+    `value` varchar(40) NULL,
+    CONSTRAINT `PK_KEYWORD` PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `workspace`
+(
+    `id`                 int NOT NULL,
+    `name`               varchar(40) NULL,
+    `latitude`           decimal(20, 17) NULL,
+    `longitude`          decimal(20, 17) NULL,
+    `rating`             decimal(3, 1) NULL,
+    `number_of_visitors` int NULL,
+    CONSTRAINT `PK_WORKSPACE` PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `workspace_keyword`
+(
+    `id`           int NOT NULL,
+    `workspace_id` int NOT NULL,
+    `keyword_id`   int NOT NULL,
+    CONSTRAINT `PK_WORKSPACE_KEYWORD` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_workspace_TO_workspace_keyword_1` FOREIGN KEY (`workspace_id`) REFERENCES `workspace` (`id`),
+    CONSTRAINT `FK_keyword_TO_workspace_keyword_1` FOREIGN KEY (`keyword_id`) REFERENCES `keyword` (`id`)
+);
+
+CREATE TABLE `member`
+(
+    `id`           int NOT NULL,
+    `email`        varchar(40) NULL,
+    `password`     varchar(40) NULL,
+    `nickname`     varchar(40) NULL,
+    `birthdate`    date NULL,
+    `position`     varchar(20) NULL,
+    `workspace_id` int NOT NULL,
+    CONSTRAINT `PK_MEMBER` PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `visited_space`
+(
+    `id`           int NOT NULL,
+    `member_id`    int NOT NULL,
+    `workspace_id` int NOT NULL,
+    `rating_sum`   decimal(3, 1) NULL,
+    `visit_count`  int NULL,
+    CONSTRAINT `PK_VISITED_SPACE` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_member_TO_visited_space_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+    CONSTRAINT `FK_workspace_TO_visited_space_1` FOREIGN KEY (`workspace_id`) REFERENCES `workspace` (`id`)
+);
+
+CREATE TABLE `visited_review`
+(
+    `id`               int NOT NULL,
+    `visited_space_id` int NOT NULL,
+    `visit_date`       date NULL,
+    `start_time`       time NULL,
+    `end_time`         time NULL,
+    `activities`       varchar(255) NULL,
+    `rating`           decimal(3, 1) NULL,
+    `review_text`      text NULL,
+    CONSTRAINT `PK_VISITED_REVIEW` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_visited_space_TO_visited_review_1` FOREIGN KEY (`visited_space_id`) REFERENCES `visited_space` (`id`)
+);
+
+CREATE TABLE `review_keyword`
+(
+    `id`         int NOT NULL,
+    `keyword_id` int NOT NULL,
+    CONSTRAINT `PK_REVIEW_KEYWORD` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_keyword_TO_review_keyword_1` FOREIGN KEY (`keyword_id`) REFERENCES `keyword` (`id`)
+);
+
+CREATE TABLE `reservation`
+(
+    `id`                int NOT NULL,
+    `member_id`         int NOT NULL,
+    `workspace_id`      int NOT NULL,
+    `visit_date`        date NULL,
+    `visit_timeslot`    varchar(10) NULL,
+    `activity`          varchar(255) NULL,
+    `activity_duration` int NULL,
+    CONSTRAINT `PK_RESERVATION` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_member_TO_reservation_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+    CONSTRAINT `FK_workspace_TO_reservation_1` FOREIGN KEY (`workspace_id`) REFERENCES `workspace` (`id`)
+);
+
+CREATE TABLE `visited_review_image`
+(
+    `id`        int NOT NULL,
+    `review_id` int NOT NULL,
+    `image_url` varchar(255) NULL,
+    CONSTRAINT `PK_VISITED_REVIEW_IMAGE` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_visited_review_TO_visited_review_image_1` FOREIGN KEY (`review_id`) REFERENCES `visited_review` (`id`)
+);
