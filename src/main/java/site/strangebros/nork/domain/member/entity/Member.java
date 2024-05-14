717fca410bbf9a35c9babcb71199ca6cd720f3e1
sootudio <kswim8914@gmail.com>
2024-05-14 17:55:53 +0900

    feat: Member 엔티티, Mapper 코드 작성

--- src/main/java/site/strangebros/nork/domain/member/entity/Member.java
+++ src/main/java/site/strangebros/nork/domain/member/entity/Member.java
@@ -1,4 +1,17 @@
 package site.strangebros.nork.domain.member.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Member {
+    private int id;
+    private String email;
+    private String password;
+    private String nickname;
+    private String birthdate;
+    private String position;
 }
+
+
