44e4d4d0d49e0e8d4b2bb79008765d691a4b34f8
김수영 <kswim8914@gmail.com>
2024-05-15 15:48:30 +0900

    fix: LoginResponse dto에 consturctor annotation 추가, Builder 관련 코드 수정

--- src/main/java/site/strangebros/nork/domain/member/service/dto/response/LoginResponse.java
+++ src/main/java/site/strangebros/nork/domain/member/service/dto/response/LoginResponse.java
@@ -2,9 +2,15 @@ package site.strangebros.nork.domain.member.service.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class LoginResponse {
     private String accessToken;
+
+    @Builder
+    public LoginResponse(String accessToken){
+        this.accessToken = accessToken;
+    }
 }
