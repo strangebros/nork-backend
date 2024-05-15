0f77324b98435be4dfdb9894fb20e6eb5e597c8f
gmelon <hsh1769@naver.com>
2024-05-15 22:37:19 +0900

    feat: Workspace Mapper 및 Entity 구현

--- src/main/java/site/strangebros/nork/domain/workspace/entity/Workspace.java
+++ src/main/java/site/strangebros/nork/domain/workspace/entity/Workspace.java
@@ -1,4 +1,33 @@
 package site.strangebros.nork.domain.workspace.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class Workspace {
+    private int id;
+    private String name;
+    private String category;
+    private Double latitude;
+    private Double longitude;
+    private String roadAddress;
+    private String imageUrl;
+    private Double rating;
+    private Integer numberOfVisitors;
+    private String poiId;
+
+    @Builder
+    public Workspace(int id, String name, String category, Double latitude, Double longitude, String roadAddress,
+                     String imageUrl, Double rating, Integer numberOfVisitors, String poiId) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.roadAddress = roadAddress;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+        this.numberOfVisitors = numberOfVisitors;
+        this.poiId = poiId;
+    }
 }
--- src/main/java/site/strangebros/nork/domain/workspace/mapper/WorkspaceMapper.java
+++ src/main/java/site/strangebros/nork/domain/workspace/mapper/WorkspaceMapper.java
@@ -1,4 +1,10 @@
 package site.strangebros.nork.domain.workspace.mapper;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import site.strangebros.nork.domain.workspace.entity.Workspace;
+
+@Mapper
 public interface WorkspaceMapper {
+    Workspace findAllByPoiIds(List<String> poiIds);
 }
