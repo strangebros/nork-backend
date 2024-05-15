612d5a6b65d9b46c97fbf36eb2cf0a60ca371abf
gmelon <hsh1769@naver.com>
2024-05-15 17:06:58 +0900

    refactor: 200, 201 응답에 대해 기본 메시지를 포함하는 객체를 만들어주는 팩토리 메서드 생성

--- src/main/java/site/strangebros/nork/global/web/dto/response/SuccessResponse.java
+++ src/main/java/site/strangebros/nork/global/web/dto/response/SuccessResponse.java
@@ -8,10 +8,19 @@ import org.springframework.http.HttpStatus;
 @Setter
 @Getter
 public class SuccessResponse<D> {
+    private static final String DEFAULT_SUCCESS_MESSAGE = "요청을 성공했습니다.";
 
     private int status;
     private String message;
     private D data;
+
+    public static <D> SuccessResponse<D> ok(D data) {
+        return new SuccessResponse<>(HttpStatus.OK, DEFAULT_SUCCESS_MESSAGE, data);
+    }
+
+    public static <D> SuccessResponse<D> created(D data) {
+        return new SuccessResponse<>(HttpStatus.CREATED, DEFAULT_SUCCESS_MESSAGE, data);
+    }
 
     @Builder
     public SuccessResponse(HttpStatus status, String message, D data) {
