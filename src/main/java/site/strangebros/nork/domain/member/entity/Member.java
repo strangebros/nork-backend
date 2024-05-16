de656c6857e043892cf4ea9a0bea4e815652ccbb
김수영 <kswim8914@gmail.com>
2024-05-16 13:37:16 +0900

    fix: member entity 수정

--- src/main/java/site/strangebros/nork/domain/member/entity/Member.java
+++ src/main/java/site/strangebros/nork/domain/member/entity/Member.java
@@ -1,17 +1,34 @@
 package site.strangebros.nork.domain.member.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Member {
     private int id;
     private String email;
     private String password;
     private String nickname;
-    private String birthdate;
+    private LocalDate birthdate;
     private String position;
+    private String role;
+
+    @Builder
+    public Member(int id, String email, String password, String nickname, LocalDate birthdate, String position, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.birthdate = birthdate;
+        this.position = position;
+        this.role = role;
+    }
 }
 
 
