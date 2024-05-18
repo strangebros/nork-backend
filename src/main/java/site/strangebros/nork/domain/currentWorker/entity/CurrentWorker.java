a441b00e7d59d02b2b1a6adab0705bfdda7b3a54
gmelon <hsh1769@naver.com>
2024-05-18 17:01:32 +0900

    refactor: CurrentWorker 가 workspace와 member의 id를 key로 갖도록 entity 수정

--- src/main/java/site/strangebros/nork/domain/currentWorker/entity/CurrentWorker.java
+++ src/main/java/site/strangebros/nork/domain/currentWorker/entity/CurrentWorker.java
@@ -2,13 +2,18 @@ package site.strangebros.nork.domain.currentWorker.entity;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
+@ToString
 @Getter
+@NoArgsConstructor
 @RedisHash("currentWorker")
 public class CurrentWorker {
     private static final int DEFAULT_LIVE_MINUTES = 10;
@@ -19,6 +24,7 @@ public class CurrentWorker {
     private String position;
     private LocalDateTime startTime;
 
+    @Getter(AccessLevel.NONE)
     @TimeToLive
     private Long timeToLive;
 
@@ -28,11 +34,15 @@ public class CurrentWorker {
 
     @Builder
     public CurrentWorker(int workspaceId, int memberId, String position, LocalDateTime startTime) {
-        this.id = workspaceId + "." + memberId;
+        this.id = buildId(workspaceId, memberId);
         this.memberId = memberId;
         this.position = position;
         this.startTime = startTime;
 
         refresh();
+    }
+
+    public static String buildId(int workspaceId, int memberId) {
+        return workspaceId + "." + memberId;
     }
 }
--- src/main/java/site/strangebros/nork/domain/currentWorker/entity/CurrentWorkerHolder.java
+++ src/main/java/site/strangebros/nork/domain/currentWorker/entity/CurrentWorkerHolder.java
@@ -1,17 +1,34 @@
 package site.strangebros.nork.domain.currentWorker.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+@ToString
 @Getter
+@NoArgsConstructor
 @RedisHash("currentWorkerHolder")
 public class CurrentWorkerHolder {
     @Id
     private int workspaceId;
     private List<String> workerIds;
+
+    public void addWorker(String workerId) {
+        if (workerIds == null) {
+            workerIds = new ArrayList<>();
+        }
+
+        if (workerIds.contains(workerId)) {
+            return;
+        }
+
+        workerIds.add(workerId);
+    }
 
     @Builder
     public CurrentWorkerHolder(int workspaceId, List<String> workerIds) {
