11f5b6add565f13336d5b84eae76ee4a6a50e350
gmelon <hsh1769@naver.com>
2024-05-19 20:51:11 +0900

    feat: keyword 전체 조회 service, mapping 로직 작성

--- src/main/java/site/strangebros/nork/domain/keyword/service/KeywordService.java
+++ src/main/java/site/strangebros/nork/domain/keyword/service/KeywordService.java
@@ -1,4 +1,21 @@
 package site.strangebros.nork.domain.keyword.service;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.strangebros.nork.domain.keyword.mapper.KeywordMapper;
+import site.strangebros.nork.domain.keyword.service.dto.response.KeywordResponse;
+
+@RequiredArgsConstructor
+@Service
 public class KeywordService {
+
+    private final KeywordMapper keywordMapper;
+
+    public List<KeywordResponse> findAll() {
+        return keywordMapper.findAll().stream()
+                .map(KeywordResponse::from)
+                .toList();
+    }
+
 }
--- src/main/java/site/strangebros/nork/domain/keyword/service/dto/response/KeywordResponse.java
+++ src/main/java/site/strangebros/nork/domain/keyword/service/dto/response/KeywordResponse.java
@@ -3,6 +3,7 @@ package site.strangebros.nork.domain.keyword.service.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import site.strangebros.nork.domain.keyword.entity.Keyword;
 
 @NoArgsConstructor
 @Getter
@@ -14,5 +15,12 @@ public class KeywordResponse {
     public KeywordResponse(int id, String value) {
         this.id = id;
         this.value = value;
+    }
+
+    public static KeywordResponse from(Keyword entity) {
+        return KeywordResponse.builder()
+                .id(entity.getId())
+                .value(entity.getValue())
+                .build();
     }
 }
