5983e48cdb5a87efe574b1b4c851689999a921b2
gmelon <hsh1769@naver.com>
2024-05-19 20:48:24 +0900

    feat: keyword 전체 조회 API Spec 명세

--- src/main/java/site/strangebros/nork/domain/keyword/service/dto/response/KeywordResponse.java
+++ src/main/java/site/strangebros/nork/domain/keyword/service/dto/response/KeywordResponse.java
@@ -1,4 +1,18 @@
 package site.strangebros.nork.domain.keyword.service.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class KeywordResponse {
+    private int id;
+    private String value;
+
+    @Builder
+    public KeywordResponse(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
 }
