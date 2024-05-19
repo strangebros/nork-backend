9202bc942ae041205f2fe16ee70b80399d5a29f6
gmelon <hsh1769@naver.com>
2024-05-19 15:14:20 +0900

    feat: workspace 단건 조회 API 구현

--- src/main/java/site/strangebros/nork/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/site/strangebros/nork/domain/workspace/controller/WorkspaceController.java
@@ -6,16 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.strangebros.nork.domain.workspace.service.WorkspaceService;
+import site.strangebros.nork.domain.workspace.service.dto.request.SearchOneWorkspaceRequest;
 import site.strangebros.nork.domain.workspace.service.dto.request.SearchWorkspaceRequest;
+import site.strangebros.nork.domain.workspace.service.dto.response.SearchOneWorkspaceResponse;
 import site.strangebros.nork.domain.workspace.service.dto.response.SearchWorkspaceResponse;
 import site.strangebros.nork.global.auth.config.CurrentMember;
 import site.strangebros.nork.global.web.dto.response.SuccessResponse;
 
 @RequiredArgsConstructor
 @RequestMapping("/workspaces")
-@RestControllerAdvice
+@RestController
 public class WorkspaceController {
 
     private final WorkspaceService workspaceService;
@@ -26,6 +29,14 @@ public class WorkspaceController {
             @ModelAttribute SearchWorkspaceRequest request
     ) {
         return SuccessResponse.ok(workspaceService.search(memberId, request));
+    }
+
+    @GetMapping(value = "/search", params = {"poiId"})
+    public SuccessResponse<SearchOneWorkspaceResponse> searchOne(
+            @CurrentMember int memberId,
+            @ModelAttribute SearchOneWorkspaceRequest request
+    ) {
+        return SuccessResponse.ok(workspaceService.searchOne(memberId, request));
     }
 
 }
