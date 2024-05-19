90778b7efa0d7e33f3b00fcae89a907a9507d567
김수영 <kswim8914@gmail.com>
2024-05-19 15:21:52 +0900

    feat: delete작업을 하기 위한 deleteByReservationId 매핑 메소드와 쿼리문 추가

--- src/main/java/site/strangebros/nork/domain/reservation/mapper/ReservationMapper.java
+++ src/main/java/site/strangebros/nork/domain/reservation/mapper/ReservationMapper.java
@@ -19,4 +19,7 @@ public interface ReservationMapper {
 
     // 유저의 상위 3개 예약 조회(모든 워크스페이스에 대하여, 현재보다 이후 날짜)
     List<Reservation> findByMemberId(int memberId);
+
+    // 작업 예약 삭제
+    void deleteByReservationId(int reservationId);
 }
