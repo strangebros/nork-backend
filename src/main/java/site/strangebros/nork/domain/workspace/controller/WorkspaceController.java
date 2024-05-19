7def1fb1c1d59c4a69b9e5d3707f74516f635521
gmelon <hsh1769@naver.com>
2024-05-19 15:42:19 +0900

    feat: poi to workspace controller 로직 구현

--- src/main/java/site/strangebros/nork/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/site/strangebros/nork/domain/workspace/controller/WorkspaceController.java
@@ -1,14 +1,21 @@
 package site.strangebros.nork.domain.workspace.controller;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import site.strangebros.nork.domain.workspace.service.WorkspaceService;
+import site.strangebros.nork.domain.workspace.service.dto.request.PoiToWorkspaceRequest;
 import site.strangebros.nork.domain.workspace.service.dto.request.SearchOneWorkspaceRequest;
 import site.strangebros.nork.domain.workspace.service.dto.request.SearchWorkspaceRequest;
 import site.strangebros.nork.domain.workspace.service.dto.response.SearchOneWorkspaceResponse;
@@ -37,6 +44,17 @@ public class WorkspaceController {
             @ModelAttribute SearchOneWorkspaceRequest request
     ) {
         return SuccessResponse.ok(workspaceService.searchOne(memberId, request));
+    }
+
+    @PostMapping
+    public ResponseEntity<SuccessResponse<?>> poiToWorkspace(@RequestBody PoiToWorkspaceRequest request)
+            throws MalformedURLException, URISyntaxException {
+        int workspaceId = workspaceService.poiToWorkspace(request);
+
+        return ResponseEntity.created(UriComponentsBuilder.fromHttpUrl("/workspaces")
+                .path("/{id}").build(workspaceId)
+                .toURL().toURI())
+                .body(SuccessResponse.created());
     }
 
 }
