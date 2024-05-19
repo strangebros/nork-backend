ceeb422eb00091692db8b34adfb307d7bd5a4bab
김수영 <kswim8914@gmail.com>
2024-05-19 19:56:57 +0900

    feat: review를 생성하는 createReview 메소드 작성

--- src/main/java/site/strangebros/nork/domain/review/controller/ReviewController.java
+++ src/main/java/site/strangebros/nork/domain/review/controller/ReviewController.java
@@ -1,4 +1,28 @@
 package site.strangebros.nork.domain.review.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import site.strangebros.nork.domain.review.service.ReviewService;
+import site.strangebros.nork.domain.review.service.dto.request.CreateRequest;
+import site.strangebros.nork.global.auth.config.CurrentMember;
+import site.strangebros.nork.global.web.dto.response.SuccessResponse;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    // 리뷰 등록
+    // WorkController로 이사갈 예정...
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    public SuccessResponse<?> createReview(@RequestBody @Valid CreateRequest createRequest, @CurrentMember Integer memberId){
+        reviewService.createReview(createRequest, memberId);
+
+        return SuccessResponse.created();
+    }
 }
--- src/main/java/site/strangebros/nork/domain/review/service/ReviewService.java
+++ src/main/java/site/strangebros/nork/domain/review/service/ReviewService.java
@@ -1,4 +1,38 @@
 package site.strangebros.nork.domain.review.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.strangebros.nork.domain.review.entity.Review;
+import site.strangebros.nork.domain.review.entity.ReviewImage;
+import site.strangebros.nork.domain.review.mapper.ReviewMapper;
+import site.strangebros.nork.domain.review.service.dto.request.CreateRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewMapper reviewMapper;
+
+    @Transactional
+    public void createReview(CreateRequest createRequest, Integer memberId) {
+        // Review Entity로 수정
+        Review createInfo = createRequest.toReview(memberId);
+
+        // DB에 저장
+        reviewMapper.create(createInfo);
+
+        // DB에 이미지 저장
+        // create에 useGeneratedKeys 속성을 사용했으므로, 방금 만든 review의 id를 바로 가져올 수 있다.
+        List<ReviewImage> images = createRequest.getImages().stream()
+                .map(image -> createRequest.toReviewImage(createInfo.getId(), image))
+                .collect(Collectors.toList());
+
+        for (ReviewImage reviewImage : images) {
+            reviewMapper.createImage(reviewImage);
+        }
+    }
 }
