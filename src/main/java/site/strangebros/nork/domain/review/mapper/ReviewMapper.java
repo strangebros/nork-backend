0e56b7a184cdbaf5b550edaa8ee387e1ed782890
김수영 <kswim8914@gmail.com>
2024-05-19 19:56:32 +0900

    feat: review를 생성할 때, db에 review와 review image를 넣는 매핑 메소드, 쿼리 작성

--- src/main/java/site/strangebros/nork/domain/review/mapper/ReviewMapper.java
+++ src/main/java/site/strangebros/nork/domain/review/mapper/ReviewMapper.java
@@ -1,4 +1,21 @@
 package site.strangebros.nork.domain.review.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import site.strangebros.nork.domain.review.entity.Review;
+import site.strangebros.nork.domain.review.entity.ReviewImage;
+
+import java.util.List;
+
+@Mapper
 public interface ReviewMapper {
+    // 리뷰 생성
+    int create(Review createInfo);
+
+    // 리뷰 이미지 생성
+    int createImage(ReviewImage createImageInfo);
+
+    // 유저의 특정 장소에 대한 모든 리뷰 가져오기.
+    List<Review> findByMemberIdAndWorkspaceId(int memberId, int workspaceId);
+
+
 }
