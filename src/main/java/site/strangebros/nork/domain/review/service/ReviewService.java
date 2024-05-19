3cd2570821ba71b6844e1880329dee88b054068b
김수영 <kswim8914@gmail.com>
2024-05-19 23:50:50 +0900

    feat: Review를 생성할 때, keyword에 대한 처리 추가

--- src/main/java/site/strangebros/nork/domain/review/service/ReviewService.java
+++ src/main/java/site/strangebros/nork/domain/review/service/ReviewService.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.strangebros.nork.domain.review.entity.Review;
 import site.strangebros.nork.domain.review.entity.ReviewImage;
+import site.strangebros.nork.domain.review.mapper.ReviewKewordMapper;
 import site.strangebros.nork.domain.review.mapper.ReviewMapper;
 import site.strangebros.nork.domain.review.service.dto.request.CreateRequest;
+import site.strangebros.nork.domain.workspace.entity.WorkspaceKeyword;
+import site.strangebros.nork.domain.workspace.mapper.WorkspaceKeywordMapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,9 +19,16 @@ import java.util.stream.Collectors;
 public class ReviewService {
 
     private final ReviewMapper reviewMapper;
+    private final ReviewKewordMapper reviewKewordMapper;
+    private final WorkspaceKeywordMapper workspaceKeywordMapper;
 
     @Transactional
     public void createReview(CreateRequest createRequest, Integer memberId) {
+        // Review keyword가 3개보다 많다면 에러 던짐
+        if(!createRequest.getKeywords().isEmpty() && createRequest.getKeywords().size() > 3){
+            throw new IllegalArgumentException("키워드는 3개 이하로 선택해주세요.");
+        }
+
         // Review Entity로 수정
         Review createInfo = createRequest.toReview(memberId);
 
@@ -30,5 +40,22 @@ public class ReviewService {
         createRequest.getImages().stream()
                 .map(image -> createRequest.toReviewImage(createInfo.getId(), image))
                 .forEach(reviewMapper::createImage);
+
+        // DB에 리뷰 키워드 저장
+        // review_keword 테이블에 추가
+        createRequest.getKeywords().stream()
+                .map(keywordId -> createRequest.toReviewKeyword(createInfo.getId(), keywordId))
+                .forEach(reviewKewordMapper::createKeyword);
+
+        // DB에 워크스페이스 키워드 저장
+        // workspace_keyword 테이블에 추가
+        createRequest.getKeywords().stream()
+                .map(keywordId -> createRequest.toWorkspaceKeyword(createInfo.getWorkspaceId(), keywordId))
+                .forEach(workspaceKeyword -> {
+                    if(workspaceKeywordMapper.findByWorkspaceIdAndKeywordId(createInfo.getWorkspaceId(), workspaceKeyword.getKeywordId()) == 0)
+                        workspaceKeywordMapper.createKeyword(workspaceKeyword);
+                    else
+                        workspaceKeywordMapper.increaseFrequency(workspaceKeyword.getWorkspaceId(), workspaceKeyword.getKeywordId());
+                });
     }
 }
