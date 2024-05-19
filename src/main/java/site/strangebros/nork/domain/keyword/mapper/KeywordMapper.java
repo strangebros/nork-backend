62548c73943c8821658a01ba7b98173a8e8a72d3
gmelon <hsh1769@naver.com>
2024-05-19 20:49:51 +0900

    feat: keyword 전체 조회 mapper 작성

--- src/main/java/site/strangebros/nork/domain/keyword/mapper/KeywordMapper.java
+++ src/main/java/site/strangebros/nork/domain/keyword/mapper/KeywordMapper.java
@@ -1,4 +1,10 @@
 package site.strangebros.nork.domain.keyword.mapper;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import site.strangebros.nork.domain.keyword.entity.Keyword;
+
+@Mapper
 public interface KeywordMapper {
+    List<Keyword> findAll();
 }
