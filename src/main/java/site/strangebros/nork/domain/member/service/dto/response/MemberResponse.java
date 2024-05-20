d781a9a108c369efbb897e6a4595a36be6ee67ca
gmelon <hsh1769@naver.com>
2024-05-20 14:18:01 +0900

    feat: id를 통한 member 조회 service 구현

--- src/main/java/site/strangebros/nork/domain/member/service/dto/response/MemberResponse.java
+++ src/main/java/site/strangebros/nork/domain/member/service/dto/response/MemberResponse.java
@@ -1,4 +1,35 @@
 package site.strangebros.nork.domain.member.service.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.strangebros.nork.domain.member.entity.Member;
+
+@NoArgsConstructor
+@Getter
 public class MemberResponse {
+    private int id;
+    private String email;
+    private String nickname;
+    private String profileImage;
+    private String role;
+
+    @Builder
+    public MemberResponse(int id, String email, String nickname, String profileImage, String role) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImage = profileImage;
+        this.role = role;
+    }
+
+    public static MemberResponse from(Member member) {
+        return MemberResponse.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .profileImage(member.getProfileImage())
+                .role(member.getRole())
+                .build();
+    }
 }
