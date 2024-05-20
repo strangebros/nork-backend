e92c6b46227b4e7354c586b4e97e0bf00594e2dc
sootudio <kswim8914@gmail.com>
2024-05-20 17:57:00 +0900

    feat: review를 조회할 때 필요한 매핑 메소드와 쿼리 작성(미완성)

--- src/main/java/site/strangebros/nork/domain/review/mapper/ReviewMapper.java
+++ src/main/java/site/strangebros/nork/domain/review/mapper/ReviewMapper.java
@@ -14,8 +14,15 @@ public interface ReviewMapper {
     // 리뷰 이미지 생성
     int createImage(ReviewImage createImageInfo);
 
-    // 유저의 특정 장소에 대한 모든 리뷰 가져오기.
-    List<Review> findByMemberIdAndWorkspaceId(int memberId, int workspaceId);
+    // 유저의 리뷰 5개씩 조회(단일 워크스페이스에 대하여) - 워크스페이스 페이지 -> 나의 리뷰 조회
+    List<Review> findByMemberIdAndWorkspaceId(Review review, int offset, int count);
 
+    // 유저의 리뷰 10개씩 조회(모든 워크스페이스에 대하여) - 마이페이지 -> 리뷰들 조회
+    List<Review> findByMemberId(Review review, int offset, int count);
 
+    // 단일 리뷰 조회(업데이트 시 정보를 불러오기 위함)
+    Review findByReviewId(int reviewId);
+
+    // 리뷰 조회 시 이미지 객체를 만들기 위해 이미지 조회
+    ReviewImage findImageByReviewIdAndImage(int reviewId, String image);
 }
