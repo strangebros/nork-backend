6ca473ea7b1bf60c291712c672bc8080dad2dadd
gmelon <hsh1769@naver.com>
2024-05-20 09:52:05 +0900

    feat: 작업 시작 및 종료 서비스 로직 구현

--- src/main/java/site/strangebros/nork/domain/work/service/WorkService.java
+++ src/main/java/site/strangebros/nork/domain/work/service/WorkService.java
@@ -4,12 +4,15 @@ import java.time.Clock;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import site.strangebros.nork.domain.currentWorker.service.CurrentWorkerService;
 import site.strangebros.nork.domain.currentWorker.service.dto.CreateWorkerRequest;
 import site.strangebros.nork.domain.member.entity.Member;
 import site.strangebros.nork.domain.member.mapper.MemberMapper;
 import site.strangebros.nork.domain.reservation.entity.Reservation;
 import site.strangebros.nork.domain.reservation.mapper.ReservationMapper;
+import site.strangebros.nork.domain.review.service.ReviewService;
+import site.strangebros.nork.domain.work.service.dto.request.WorkEndRequest;
 import site.strangebros.nork.domain.work.service.dto.request.WorkStartRequest;
 
 @RequiredArgsConstructor
@@ -17,13 +20,19 @@ import site.strangebros.nork.domain.work.service.dto.request.WorkStartRequest;
 public class WorkService {
 
     private final CurrentWorkerService currentWorkerService;
+    private final ReviewService reviewService;
     private final MemberMapper memberMapper;
     private final ReservationMapper reservationMapper;
     private final Clock clock;
 
+    @Transactional
     public void start(int memberId, WorkStartRequest request) {
-        Reservation reservation = findReservation(request);
+        Reservation reservation = findReservation(request.getReservationId());
         Member member = findMember(memberId);
+
+        if (reservation.getMemberId() != memberId) {
+            throw new IllegalArgumentException("회원이 생성한 예약이 아닙니다.");
+        }
 
         CreateWorkerRequest createWorkerRequest = CreateWorkerRequest.builder()
                 .reservationId(request.getReservationId())
@@ -34,8 +43,21 @@ public class WorkService {
         currentWorkerService.createWorker(memberId, createWorkerRequest);
     }
 
-    private Reservation findReservation(WorkStartRequest request) {
-        Reservation reservation = reservationMapper.findByReservationId(request.getReservationId());
+    @Transactional
+    public void end(int memberId, WorkEndRequest request) {
+        Reservation reservation = findReservation(request.getReservationId());
+
+        if (reservation.getMemberId() != memberId) {
+            throw new IllegalArgumentException("회원이 생성한 예약이 아닙니다.");
+        }
+
+        currentWorkerService.deleteWorker(reservation.getId());
+        reviewService.createReview(request.toReviewCreateRequest(), memberId);
+        reservationMapper.deleteByReservationId(request.getReservationId());
+    }
+
+    private Reservation findReservation(int reservationId) {
+        Reservation reservation = reservationMapper.findByReservationId(reservationId);
         if (reservation == null) {
             throw new IllegalArgumentException("존재하지 않는 reservation id 입니다.");
         }
--- src/main/java/site/strangebros/nork/domain/work/service/dto/request/WorkEndRequest.java
+++ src/main/java/site/strangebros/nork/domain/work/service/dto/request/WorkEndRequest.java
@@ -1,16 +1,66 @@
 package site.strangebros.nork.domain.work.service.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import site.strangebros.nork.domain.review.service.dto.request.CreateRequest;
+import site.strangebros.nork.domain.review.service.dto.request.ReviewRequest;
 
 @NoArgsConstructor
 @Getter
 public class WorkEndRequest {
+    @NotNull(message = "종료할 작업의 예약 id를 설정해 주세요.")
     private int reservationId;
 
+    // 리뷰 작성 관련 필드
+    private int workspaceId;
+
+    @NotNull(message = "작업 시작 시간을 설정해 주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime startDatetime;
+
+    @NotNull(message = "작업 종료 시간을 설정해 주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime endDatetime;
+
+    private String activity;
+    private Double rating;
+    private String reviewText;
+
+    private List<String> images = Collections.emptyList(); // Base64 encoded images
+
+    private List<Integer> keywords = Collections.emptyList(); // 키워드 id들
+
     @Builder
-    public WorkEndRequest(int reservationId) {
+    public WorkEndRequest(int reservationId, int workspaceId, LocalDateTime startDatetime, LocalDateTime endDatetime,
+                          String activity, Double rating, String reviewText, List<String> images,
+                          List<Integer> keywords) {
         this.reservationId = reservationId;
+        this.workspaceId = workspaceId;
+        this.startDatetime = startDatetime;
+        this.endDatetime = endDatetime;
+        this.activity = activity;
+        this.rating = rating;
+        this.reviewText = reviewText;
+        this.images = images;
+        this.keywords = keywords;
+    }
+
+    public CreateRequest toReviewCreateRequest() {
+        return CreateRequest.builder()
+                .workspaceId(workspaceId)
+                .startDatetime(startDatetime)
+                .endDatetime(endDatetime)
+                .activity(activity)
+                .rating(rating)
+                .reviewText(reviewText)
+                .images(images)
+                .keywords(keywords)
+                .build();
     }
 }
