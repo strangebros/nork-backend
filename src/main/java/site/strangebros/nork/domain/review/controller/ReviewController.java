90b974efc6c9f33739c9708ccfd5a4c0b566f251
sootudio <kswim8914@gmail.com>
2024-05-20 17:57:22 +0900

    feat: 상황별 review 조회 메소드들 추가

--- src/main/java/site/strangebros/nork/domain/review/controller/ReviewController.java
+++ src/main/java/site/strangebros/nork/domain/review/controller/ReviewController.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import site.strangebros.nork.domain.review.service.ReviewService;
 import site.strangebros.nork.domain.review.service.dto.request.CreateRequest;
+import site.strangebros.nork.domain.review.service.dto.request.ReadRequest;
+import site.strangebros.nork.domain.review.service.dto.response.ReadResponse;
 import site.strangebros.nork.global.auth.config.CurrentMember;
 import site.strangebros.nork.global.web.dto.response.SuccessResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reviews")
@@ -24,5 +28,30 @@ public class ReviewController {
         reviewService.createReview(createRequest, memberId);
 
         return SuccessResponse.created();
+    }
+
+    // 리뷰 조회
+    @GetMapping()
+    public SuccessResponse<List<ReadResponse>> readReviews(@ModelAttribute ReadRequest readRequest, @CurrentMember Integer memberId){
+        List<ReadResponse> reviews;
+
+        // 유저의 리뷰 5개씩 조회(단일 워크스페이스에 대하여)
+        if(readRequest.getWorkspaceId() != -1){
+           reviews = reviewService.readWorkspaceReview(readRequest, memberId);
+        }
+        // 유저의 리뷰 10개씩 조회(모든 워크스페이스에 대하여) - 마이페이지 -> 리뷰들 조회
+        else{
+            reviews = reviewService.readReview(readRequest, memberId);
+        }
+
+        return SuccessResponse.ok(reviews);
+    }
+
+    // 단일 리뷰 조회(업데이트 시 정보를 불러오기 위함)
+    @GetMapping("/{id}")
+    public SuccessResponse<ReadResponse> readReviewDetail(@PathVariable("id") int reviewId){
+        ReadResponse review = reviewService.readReviewDetail(reviewId);
+
+        return SuccessResponse.ok(review);
     }
 }
