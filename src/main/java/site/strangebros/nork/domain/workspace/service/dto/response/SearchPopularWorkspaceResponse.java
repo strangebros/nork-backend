cfc4ad2352cbc6a84cda21ff6d7d7cc0037aca01
김수영 <kswim8914@gmail.com>
2024-05-22 13:30:22 +0900

    feat: 인기있는 여행지를 반환하는 응답 DTO 생성

---
+++ src/main/java/site/strangebros/nork/domain/workspace/service/dto/response/SearchPopularWorkspaceResponse.java
@@ -0,0 +1,42 @@
+package site.strangebros.nork.domain.workspace.service.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class SearchPopularWorkspaceResponse {
+
+    private Integer id;
+    private String name;
+    private String category;
+    private Double latitude;
+    private Double longitude;
+    private String roadAddress;
+    private Double rating;
+    private String poiId;
+
+    private LocalDateTime recentVisitedDate;
+
+    private List<String> keywords;
+
+    @Builder
+    public SearchPopularWorkspaceResponse(Integer id, String name, String category, Double latitude, Double longitude, String roadAddress, Double rating, String poiId, LocalDateTime recentVisitedDate, List<String> keywords) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.roadAddress = roadAddress;
+        this.rating = rating;
+        this.poiId = poiId;
+        this.recentVisitedDate = recentVisitedDate;
+        this.keywords = keywords;
+    }
+}
